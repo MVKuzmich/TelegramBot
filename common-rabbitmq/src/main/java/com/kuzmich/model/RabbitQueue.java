47aca3c0bb529bbdbc22063a08c15a5d0cd3ce0b
Mikhail Kuzmich <kmv85@mail.ru>
2023-12-14 14:50:04 +0300

    add rabbitmq module

---
+++ common-rabbitmq/src/main/java/com/kuzmich/model/RabbitQueue.java
@@ -0,0 +1,16 @@
+package com.kuzmich.model;
+
+import lombok.Getter;
+
+@Getter
+public enum RabbitQueue {
+    DOC_MESSAGE_UPDATE("doc_message_update"), 
+    PHOTO_MESSAGE_UPDATE("photo_message_update"),
+    TEXT_MESSAGE_UPDATE("text_message_update"), 
+    ANSWER_MESSAGE("answer_message");
+
+    private final String name;
+    RabbitQueue(String name) {
+        this.name = name;
+    }
+}
