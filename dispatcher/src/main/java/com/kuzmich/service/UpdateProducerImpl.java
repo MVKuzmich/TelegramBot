300bf50018e15b300708fe18f9880d2b118f3fc0
Mikhail Kuzmich <kmv85@mail.ru>
2023-12-15 11:42:56 +0300

    add rabbitmq

--- dispatcher/src/main/java/com/kuzmich/service/UpdateProducerImpl.java
+++ dispatcher/src/main/java/com/kuzmich/service/UpdateProducerImpl.java
@@ -1,15 +1,21 @@
 package com.kuzmich.service;
 
 import com.kuzmich.model.RabbitQueue;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class UpdateProducerImpl implements UpdateProducer{
+
+    private final RabbitTemplate rabbitTemplate;
     @Override
     public void produce(RabbitQueue rabbitQueue, Update update) {
         log.info(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue.getName(), update);
     }
 }
